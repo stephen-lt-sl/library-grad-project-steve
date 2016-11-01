99cabd68ef64ecb7cc64d85dd768f158c574b0ad
Stephen Tozer <stozer@scottlogic.co.uk>
2016-11-01 15:48:03 +0000

    Add pre-filled book repository

---
+++ library-grad-project-tests/Repos/FilledBookRepositoryTests.cs
@@ -0,0 +1,128 @@
+﻿using LibraryGradProject.Models;
+using LibraryGradProject.Repos;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using DeepEqual.Syntax;
+
+namespace LibraryGradProjectTests.Repos
+{
+    public class FilledBookRepositoryTests
+    {
+        private Book bookA = new Book
+        {
+            Id = 0,
+            ISBN = "ABCD1234",
+            Title = "Cool book",
+            Author = "Book Writer",
+            PublishDate = "01/01/1992",
+        };
+
+        private Book bookB = new Book
+        {
+            Id = 1,
+            ISBN = "EFGH5678",
+            Title = "Lame Book",
+            Author = "Booker Writeman",
+            PublishDate = "02/03/1994",
+        };
+
+        [Fact]
+        public void New_Filled_Book_Repository_Contains_2_Books()
+        {
+            // Arrange
+            FilledBookRepository repo = new FilledBookRepository();
+
+            // Act
+            IEnumerable<Book> books = repo.GetAll();
+
+            // Assert
+            books.ShouldDeepEqual(new Book[] { bookA, bookB });
+        }
+
+        [Fact]
+        public void Add_Inserts_New_Book()
+        {
+            // Arrange
+            FilledBookRepository repo = new FilledBookRepository();
+            Book newBook = new Book() { Title = "Test" };
+
+            // Act
+            repo.Add(newBook);
+            IEnumerable<Book> books = repo.GetAll();
+
+            // Assert
+            books.ShouldDeepEqual(new Book[] { bookA, bookB, newBook });
+        }
+
+        [Fact]
+        public void Add_Sets_New_Id()
+        {
+            // Arrange
+            FilledBookRepository repo = new FilledBookRepository();
+            Book newBook = new Book() { Title = "Test" };
+
+            // Act
+            repo.Add(newBook);
+            IEnumerable<Book> books = repo.GetAll();
+
+            
+            // Asert
+            Assert.Equal(2, books.Last().Id);
+        }
+
+        [Fact]
+        public void Get_Returns_Specific_Book()
+        {
+            // Arrange
+            FilledBookRepository repo = new FilledBookRepository();
+            Book newBook1 = new Book() { Id = 0, Title = "Test1" };
+            Book newBook2 = new Book() { Id = 1, Title = "Test2" };
+            repo.Add(newBook1);
+            repo.Add(newBook2);
+
+            // Act
+            Book book = repo.Get(3);
+
+            // Asert
+            Assert.Equal(newBook2, book);
+        }
+
+        [Fact]
+        public void Get_All_Returns_All_Books()
+        {
+            // Arrange
+            FilledBookRepository repo = new FilledBookRepository();
+            Book newBook1 = new Book() { Title = "Test1" };
+            Book newBook2 = new Book() { Title = "Test2" };
+            repo.Add(newBook1);
+            repo.Add(newBook2);
+
+            // Act
+            IEnumerable<Book> books = repo.GetAll();
+
+            // Asert
+            books.ShouldDeepEqual(new Book[] { bookA, bookB, newBook1, newBook2 });
+        }
+
+        [Fact]
+        public void Delete_Removes_Correct_Book()
+        {
+            // Arrange
+            FilledBookRepository repo = new FilledBookRepository();
+            Book newBook1 = new Book() { Title = "Test1" };
+            Book newBook2 = new Book() { Title = "Test2" };
+            Book newBook3 = new Book() { Title = "Test3" };
+            repo.Add(newBook1);
+            repo.Add(newBook2);
+            repo.Add(newBook3);
+
+            // Act
+            repo.Remove(3);
+            IEnumerable<Book> books = repo.GetAll();
+
+            // Asert
+            books.ShouldDeepEqual(new Book[] { bookA, bookB, newBook1, newBook3 });
+        }
+    }
+}
---
+++ library-grad-project/Repos/FilledBookRepository.cs
@@ -0,0 +1,53 @@
+﻿using LibraryGradProject.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LibraryGradProject.Repos
+{
+    public class FilledBookRepository : IRepository<Book>
+    {
+        public FilledBookRepository()
+        {
+            Book bookA = new Book
+            {
+                ISBN = "ABCD1234",
+                Title = "Cool book",
+                Author = "Book Writer",
+                PublishDate = "01/01/1992",
+            };
+            Book bookB = new Book
+            {
+                ISBN = "EFGH5678",
+                Title = "Lame Book",
+                Author = "Booker Writeman",
+                PublishDate = "02/03/1994",
+            };
+            Add(bookA);
+            Add(bookB);
+        }
+
+        private List<Book> _bookCollection = new List<Book>();
+
+        public void Add(Book entity)
+        {
+            entity.Id = _bookCollection.Count;
+            _bookCollection.Add(entity);
+        }
+
+        public IEnumerable<Book> GetAll()
+        {
+            return _bookCollection;
+        }
+
+        public Book Get(int id)
+        {
+            return _bookCollection.Where(book => book.Id == id).SingleOrDefault();
+        }
+
+        public void Remove(int id)
+        {
+            Book bookToRemove = Get(id);
+            _bookCollection.Remove(bookToRemove);
+        }
+    }
+}
