87160865a10cf4a356532f78b217a94f0eeffad3
Stephen Tozer <stozer@scottlogic.co.uk>
2016-11-01 15:49:27 +0000

    Add reservation repository

---
+++ library-grad-project-tests/Repos/ReservationRepositoryTests.cs
@@ -0,0 +1,139 @@
+﻿using LibraryGradProject.Models;
+using LibraryGradProject.Repos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace LibraryGradProjectTests.Repos
+{
+    public class ReservationRepositoryTests
+    {
+        [Fact]
+        public void New_Reservation_Repository_Is_Empty()
+        {
+            // Arrange
+            ReservationRepository repo = new ReservationRepository();
+
+            // Act
+            IEnumerable<Reservation> reservations = repo.GetAll();
+
+            // Asert
+            Assert.Empty(reservations);
+        }
+
+        [Fact]
+        public void Add_Inserts_New_Reservation()
+        {
+            // Arrange
+            ReservationRepository repo = new ReservationRepository();
+            Reservation newReservation = new Reservation() { BeginDate = new DateTime() };
+
+            // Act
+            repo.Add(newReservation);
+            IEnumerable<Reservation> reservations = repo.GetAll();
+
+            // Asert
+            Assert.Equal(new Reservation[] { newReservation }, reservations.ToArray());
+        }
+
+        [Fact]
+        public void Add_Sets_New_Id()
+        {
+            // Arrange
+            ReservationRepository repo = new ReservationRepository();
+            Reservation newReservation = new Reservation() { BookId = 0 };
+
+            // Act
+            repo.Add(newReservation);
+            IEnumerable<Reservation> reservations = repo.GetAll();
+
+            // Asert
+            Assert.Equal(0, reservations.First().Id);
+        }
+
+        [Fact]
+        public void Add_Not_Throws_If_Reservation_Slot_Not_In_Use()
+        {
+            // Arrange
+            ReservationRepository repo = new ReservationRepository();
+            Reservation newReservation1 = new Reservation() { BeginDate = new DateTime(100), EndDate = new DateTime(500) };
+            Reservation newReservation2 = new Reservation() { BeginDate = new DateTime(600), EndDate = new DateTime(700) };
+
+            // Act
+            repo.Add(newReservation1);
+
+            // Assert
+            repo.Add(newReservation2);
+        }
+
+        [Fact]
+        public void Add_Throws_If_Reservation_Slot_In_Use()
+        {
+            // Arrange
+            ReservationRepository repo = new ReservationRepository();
+            Reservation newReservation1 = new Reservation() { BeginDate = new DateTime(100), EndDate = new DateTime(500) };
+            Reservation newReservation2 = new Reservation() { BeginDate = new DateTime(400), EndDate = new DateTime(700) };
+
+            // Act
+            repo.Add(newReservation1);
+
+            // Asert
+            Assert.Throws<System.Exception>(() => repo.Add(newReservation2));
+        }
+
+        [Fact]
+        public void Get_Returns_Specific_Reservation()
+        {
+            // Arrange
+            ReservationRepository repo = new ReservationRepository();
+            Reservation newReservation1 = new Reservation() { Id = 0, BookId = 0 };
+            Reservation newReservation2 = new Reservation() { Id = 1, BookId = 1 };
+            repo.Add(newReservation1);
+            repo.Add(newReservation2);
+
+            // Act
+            Reservation reservation = repo.Get(1);
+
+            // Asert
+            Assert.Equal(newReservation2, reservation);
+        }
+
+        [Fact]
+        public void Get_All_Returns_All_Reservations()
+        {
+            // Arrange
+            ReservationRepository repo = new ReservationRepository();
+            Reservation newReservation1 = new Reservation() { BookId = 0 };
+            Reservation newReservation2 = new Reservation() { BookId = 1 };
+            repo.Add(newReservation1);
+            repo.Add(newReservation2);
+
+            // Act
+            IEnumerable<Reservation> reservations = repo.GetAll();
+
+            // Asert
+            Assert.Equal(new Reservation[] { newReservation1, newReservation2 }, reservations.ToArray());
+        }
+
+        [Fact]
+        public void Delete_Removes_Correct_Reservation()
+        {
+            // Arrange
+            ReservationRepository repo = new ReservationRepository();
+            Reservation newReservation1 = new Reservation() { BookId = 0 };
+            Reservation newReservation2 = new Reservation() { BookId = 1 };
+            Reservation newReservation3 = new Reservation() { BookId = 2 };
+            repo.Add(newReservation1);
+            repo.Add(newReservation2);
+            repo.Add(newReservation3);
+
+            // Act
+            repo.Remove(1);
+            IEnumerable<Reservation> reservations = repo.GetAll();
+
+            // Asert
+            Assert.Equal(new Reservation[] { newReservation1, newReservation3 }, reservations.ToArray());
+        }
+    }
+}
---
+++ library-grad-project/Repos/ReservationRepository.cs
@@ -0,0 +1,40 @@
+﻿using LibraryGradProject.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LibraryGradProject.Repos
+{
+    public class ReservationRepository : IRepository<Reservation>
+    {
+        private List<Reservation> _reservations = new List<Reservation>();
+
+        public void Add(Reservation entity)
+        {
+            IEnumerable<Reservation> overlappingReservations = _reservations
+                .Where(reservation => reservation.EndDate > entity.BeginDate)
+                .Where(reservation => reservation.BeginDate < entity.EndDate);
+            if (overlappingReservations.Count() > 0)
+            {
+                throw new System.Exception("Reservation timeslot is already reserved.");
+            }
+            entity.Id = _reservations.Count;
+            _reservations.Add(entity);
+        }
+
+        public IEnumerable<Reservation> GetAll()
+        {
+            return _reservations;
+        }
+
+        public Reservation Get(int id)
+        {
+            return _reservations.Where(reservation => reservation.Id == id).SingleOrDefault();
+        }
+
+        public void Remove(int id)
+        {
+            Reservation reservationToRemove = Get(id);
+            _reservations.Remove(reservationToRemove);
+        }
+    }
+}
