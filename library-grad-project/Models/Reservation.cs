ba5a619e325b31c5273ec6769bbfc6db1d9c5b8b
Stephen Tozer <stozer@scottlogic.co.uk>
2016-11-01 15:49:12 +0000

    Add reservation model

---
+++ library-grad-project/Models/Reservation.cs
@@ -0,0 +1,10 @@
+﻿namespace LibraryGradProject.Models
+{
+    public class Reservation
+    {
+        public int Id { get; set; }
+        public int BookId { get; set; }
+        public System.DateTime BeginDate { get; set; }
+        public System.DateTime EndDate { get; set; }
+    }
+}
