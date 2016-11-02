53055e7d9a5763199f234db7172074d5ce593e05
Stephen Tozer <stozer@scottlogic.co.uk>
2016-11-02 11:29:40 +0000

    Create mock library context for tests

---
+++ library-grad-project-tests/Mocks/MockLibraryContext.cs
@@ -0,0 +1,32 @@
+﻿using System.Data.Entity;
+using LibraryGradProject.Models;
+using LibraryGradProject.Contexts;
+using FakeDbSet;
+
+namespace LibraryGradProjectTests.Mocks
+{
+    class MockLibraryContext : ILibraryContext
+    {
+        public IDbSet<Book> Books { get; set; }
+        public IDbSet<Reservation> Reservations { get; set; }
+
+        public MockLibraryContext()
+        {
+            Books = new InMemoryDbSet<Book>();
+            Reservations = new InMemoryDbSet<Reservation>();
+        }
+
+        public int SaveChanges()
+        {
+            int changeCount = 0;
+            changeCount += DbSetHelper.IncrementPrimaryKey<Book>(b => b.Id, Books);
+            changeCount += DbSetHelper.IncrementPrimaryKey<Reservation>(r => r.Id, Reservations);
+            return changeCount;
+        }
+
+        public void Dispose()
+        {
+
+        }
+    }
+}
